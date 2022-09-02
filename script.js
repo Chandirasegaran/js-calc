ce93f136e735aede7d5ea37c9f4b436e15214f48
Chandirasegaran <72871345+Chandirasegaran@users.noreply.github.com>
2022-09-02 23:20:50 +0530

    Initial Upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function docal(val){
+    document.getElementById('disans').value+=val
+    return val
+}
+function solve(){
+    let a=document.getElementById('disans').value
+    let b=eval(a)
+    document.getElementById('disans').value=b
+    return y
+}
+function clearScr(){
+    document.getElementById('disans').value=''
+}
